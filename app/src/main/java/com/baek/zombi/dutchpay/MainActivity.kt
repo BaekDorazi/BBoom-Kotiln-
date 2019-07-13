250b2c7ffab6510db21ede01b4d1cc79ab2e0064
BaekDorazi <qorehgud@gmail.com>
2019-07-14 02:13:23 +0900

    기능추가 / firebase의 admob 기능 추가
    화면상으로 보이지 않음.

--- app/src/main/java/com/baek/zombi/dutchpay/MainActivity.kt
+++ app/src/main/java/com/baek/zombi/dutchpay/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.baek.zombi.dutchpay
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mAdView: AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this, getString(R.string.adAppId))
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
